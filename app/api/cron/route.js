f98cc7b72bcc561160c98595847ac4b4d5880390
censam <wgsampath@gmail.com>
2023-11-06 09:33:26 +0530

    changed maxDuration

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -9,7 +9,7 @@ import {
 } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300;
+export const maxDuration = 10;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
